8cb7dc5bb30599a94d19e61654ae4406b3591630
marvinick <marviluw@gmail.com>
2015-08-09 19:38:00 -0700

    first test

---
+++ src/TitleCaseGenerator.php
@@ -0,0 +1,15 @@
+<?php
+
+    class TitleCaseGenerator {
+
+      function MakeTitleCase ($input_title) {
+          $input_array_of_words = explode (" ", $input_title);
+          $output_titlecased = array();
+          foreach ($input_array_of_words as $word) {
+            array_push($output_titlecased, ucfirst($word));
+          }
+          return implode(" ", $output_titlecased);
+      }
+    }
+
+?>
